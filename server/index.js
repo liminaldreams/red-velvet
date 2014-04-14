e4c47ce4cfe627d75d8a05eb232fde542aa479ff
Cheng Xing <xc561@hotmail.com>
2014-04-13 22:52:54 -0400

    setting up heroku

--- server/index.js
+++ server/index.js
@@ -3,14 +3,21 @@ var Resource = require('express-resource');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ChromeExt = require('./chromeext/model');
+
+var DATABASES = {
+    local: 'mongodb://localhost/red-velvet',
+    heroku: 'mongodb://admin:mongoDB@oceanic.mongohq.com:10043/app24111546'
+}
+var db_url = heroku;
+
 var app = express();
-var port = 3700;
+var port = Number(process.env.PORT || 5000);;
 
 // Express Setup
 app.use(bodyParser.json());      // JSON parser
 
 // Database Setup
-mongoose.connect('mongodb://localhost/red-velvet')
+mongoose.connect(db_url)
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
