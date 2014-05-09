2198755d56e2cad2ef233ba4d70e76f015b03ca5
Irene Kuo <irenekuo94@hotmail.com>
2014-05-09 13:10:08 -0400

    start of chrome extension
    not working yet

---
+++ chrome extension/prototype1.js
@@ -0,0 +1,28 @@
+//chrome.alarms.create("run", {periodInMinutes: 1});
+
+// create('name';, {
+// 	when: Number,
+// 	delayInMinutes: 3,
+//  periodInMinutes: 1,
+// });
+
+console.logCopy = console.log.bind(console);
+
+console.log = function(data) {
+	var timestamp = '[' + Date.now() + ']';
+	this.logCopy(timestamp, data);
+};
+
+console.log(timestamp);
+
+
+
+// create object with alarmInfo? then call it in chrome.alarms.create("run", object.periodInMinutes);
+// set it so .js file runs every minute for now
+
+// get URL of current tab, time stamp of when tab is opened, time stamp of when going to a new
+// tab, time stamp of when tab is closed
+
+// get URL for when open and close plus time
+
+// send info to server
