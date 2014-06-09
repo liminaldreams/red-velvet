6690ccdabc2ccf6543131e87e4e9eb1c65c08fc6
Irene Kuo <irenekuo94@hotmail.com>
2014-06-08 21:27:31 -0700

    added some xmlhttprequest stuff

--- chrome extension/prototype1.js
+++ chrome extension/prototype1.js
@@ -1,32 +1,24 @@
-//chrome.alarms.create("run", {periodInMinutes: 1});
 
-// create('name';, {
-// 	when: Number,
-// 	delayInMinutes: 3,
-//  periodInMinutes: 1,
-// });
+// create an XMLHttpRequest Object
+var xml = new XMLHttpRequest();
 
-// console.logCopy = console.log.bind(console);
+// use tab events: onCreated, onActivated, ...etc.
+// chrome.tabs.onActivated.addListener(function(activeInfo){
+// 	activeInfo.tabId
+// })
 
-// console.log = function(data) {
-// 	var timestamp = '[' + Date.now() + ']';
-// 	this.logCopy(timestamp, data);
-// };
+chrome.tabs.onUpdated.addListener(function(null, null, tab) {
+	xml.open("POST", tab.url, true);
+	var time = Date.now();
+	var t = time.toString();
+	xml.send(t);
+})
 
-// console.log(timestamp);
-
+// test for popup, use Date.now() for UNIX time
 chrome.tabs.getSelected(null, function(tab) {
     document.getElementById('currentLink').innerHTML = tab.url;
-    //myFunction(tab.url);
 });
 
-// function myFunction(tablink) {
-//   // do stuff here
-//   console.log(tablink);
-// }
-
-// create object with alarmInfo? then call it in chrome.alarms.create("run", object.periodInMinutes);
-// set it so .js file runs every minute for now
 
 // get URL of current tab, time stamp of when tab is opened, time stamp of when going to a new
 // tab, time stamp of when tab is closed
