134a78e75377e25d3c9499a31b282ceca1dc8c3b
Irene Kuo <irenekuo94@hotmail.com>
2014-06-13 21:18:21 -0700

    outlined tab methods; need to finish implementing

--- chrome extension/prototype1.js
+++ chrome extension/prototype1.js
@@ -1,27 +1,58 @@
+// array of IDs; even indices are tab.tabId, odd indices are server IDs
+// store in memory current session tabId - IDs that server will be sending back
+var idArray = [];
 
-// create an XMLHttpRequest Object
-var xml = new XMLHttpRequest();
+var currentID;
+
 
 // use tab events: onCreated, onActivated, ...etc.
-// chrome.tabs.onActivated.addListener(function(activeInfo){
-// 	activeInfo.tabId
-// })
 
-chrome.tabs.onCreated.addListener(function(tab) {
-	xml.open("POST", tab.url, true);
-	var time = Date.now();
-	var t = time.toString();
-	xml.send(t);
+// when the active tab in a window changes
+chrome.tabs.onActivated.addListener(function(activeInfo){
+	// post end_time of previous currentID then update currentID
+	// create new post to database
+	activeInfo.tabId
 })
 
+// when a new tab is created
+chrome.tabs.onCreated.addListener(function(tab) {
+	// create new post to database
+	var data = {
+		"url" : tab.url,
+		"start_time" : Date.now(),
+		"end_time" :
+	};
+
+
+	// xml.open("POST", tab.url, true);
+	// var time = Date.now();
+	// var t = time.toString();
+	// xml.send(t);
+})
+
+// when a tab is updated
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (tab.url != changeInfo.url) {
-		xml.open("POST", changeInfo.url, true);
-		var time = Date.now();
-		var t = time.toString();
-		xml.send(t);
+	// if url changes, post end_time of previous currentID then update currentID
+	// create new post to database
+	// else ignore
+	if (currentID != changeInfo.url) {
+		
+		//posted data to server already
+		currentID = changeInfo.url;
+
+		// xml.open("POST", changeInfo.url, true);
+		// var time = Date.now();
+		// var t = time.toString();
+		// xml.send(t);
 	}
 })
+
+// when a tab is closed
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+	// find ID corresponding to tabID in idArray, update end_time
+
+})
+
 
 // test for popup, use Date.now() for UNIX time
 chrome.tabs.getSelected(null, function(tab) {
@@ -29,9 +60,5 @@ chrome.tabs.getSelected(null, function(tab) {
 });
 
 
-// get URL of current tab, time stamp of when tab is opened, time stamp of when going to a new
-// tab, time stamp of when tab is closed
 
-// get URL for when open and close plus time
-
-// send info to server
+// send info to server - http://red-velvet-proto.herokuapp.com/chromeext
