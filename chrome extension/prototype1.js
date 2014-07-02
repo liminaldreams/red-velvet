4fa376ef97bf74159a776f44870e30dbbdef5ffc
Irene Kuo <irenekuo94@hotmail.com>
2014-07-01 22:15:26 -0700

    working on k-v ID pairs

--- chrome extension/prototype1.js
+++ chrome extension/prototype1.js
@@ -1,95 +1,26 @@
-// array of IDs; even indices are tab.tabId, odd indices are server IDs
-// store in memory current session tabId - IDs that server will be sending back
-var ids = [];
+// function printTabID(Tab tab) {
+// 	var tabURL = tab.url;
+// 	document.getElementById('output').innerHTML += (tabURL);
+// }
 
-var currentID;
-
-// create xmlhttprequest
-var xml = new XMLHttpRequest();
-
-
-// use tab events: onCreated, onActivated, ...etc.
-
-// when the active tab in a window changes
-// chrome.tabs.onActivated.addListener(function(activeInfo){
-// 	// post end_time of previous currentID then update currentID
-	
-
-// 	// create new post to database
-// 	ids[ids.length] = activeInfo.tabId;
-// 	// need to get tab url from server?
-// })
-
-// when a new tab is created
-chrome.tabs.onCreated.addListener(function(tab) {
-	// create new post to database
-	var data = {
-		"url" : tab.url,
-		"start_time" : Date.now(),
-		"end_time" : 0
-	};
-	ids[ids.length] = tab.id;
-
-	xml.open("POST", "http://red-velvet-proto.herokuapp.com/chromeext", true);
-	xml.setRequestHeader("Content-type", "application/json");
-	xml.onreadystatechange = function () { //Call a function when the state changes.
-	    if (xml.readyState == 4 && xml.status == 200) {
-	    	alert(xml.responseText);
-	    	document.getElementById('currentLink').innerText = tab.id;
-	        ids[ids.length] = xml.responseText;
-	    }
-	}
-	var parameters = JSON.stringify(data);
-	xml.send(parameters);
-})
-
-// when a tab is updated
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-// 	// if url changes, post end_time of previous currentID then update currentID
-// 	// create new post to database
-// 	// else ignore
-// 	if (currentID != changeInfo.url) {
-		
-// 		//posted data to server already
-// 		currentID = changeInfo.url;
-
-// 		// xml.open("POST", changeInfo.url, true);
-// 		// var time = Date.now();
-// 		// var t = time.toString();
-// 		// xml.send(t);
-// 	}
-// })
-
-// when a tab is closed
-// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-// 	// find ID corresponding to tabID in ids, update end_time
-
-// })
+// chrome.tabs.onCreated.addListener(printTabID(self));
+// chrome.tabs.onUpdated.addListener(printTabID(self));
 
 
-
-//test for popup, use Date.now() for UNIX time
-// chrome.tabs.getSelected(null, function(tab) {
-//     document.getElementById('currentLink').innerHTML = tab.url;
-//     document.getElementById('currentLink').innerHTML += " ";
-//     document.getElementById('currentLink').innerHTML += Date.now();
+// chrome.tabs.getCurrent(function(tab) {
+// 	console.log(tab.url);
 // });
 
-
-//test for GET on selecting tab
-chrome.tabs.getSelected(null, function(tab) {
-	// create xmlhttprequest
-	var xml = new XMLHttpRequest();
-
-	xml.open("GET", "http://red-velvet-proto.herokuapp.com/chromeext", true);
-	xml.onreadystatechange = function () { //Call a function when the state changes.
-	    if (xml.readyState == 4 && xml.status == 200) {
-	    	alert(xml.responseText);
-	    	//document.getElementById('currentLink').innerText = tab.id;
-	    }
-	}
-	xml.send();
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
 });
 
+// create object with alarmInfo? then call it in chrome.alarms.create("run", object.periodInMinutes);
+// set it so .js file runs every minute for now
 
-// send info to server - http://red-velvet-proto.herokuapp.com/chromeext
+// get URL of current tab, time stamp of when tab is opened, time stamp of when going to a new
+// tab, time stamp of when tab is closed
+
+// get URL for when open and close plus time
+
+// send info to server
