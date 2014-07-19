bc1d2abfa7a76bb1c68e60d2921fc18f0cad9fbc
Irene Kuo <irenekuo94@hotmail.com>
2014-07-19 14:38:45 -0700

    chrome ext pretty much working

--- chrome extension/prototype1.js
+++ chrome extension/prototype1.js
@@ -37,7 +37,7 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 			"start_time" : Date.now(),
 			"end_time" : 0
 		};
-    	currentID = tab.id;
+    	//currentID = tab.id;
 
     	xml.open("POST", "http://red-velvet-proto.herokuapp.com/chromeext", true);
 		xml.setRequestHeader("Content-type", "application/json");
@@ -57,46 +57,87 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 
 // when a new tab is created
 chrome.tabs.onCreated.addListener(function(tab) {
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		if (changeInfo.status == "complete") {
+			// create new post to database
+			var data = {
+				"url" : tab.url,
+				"start_time" : Date.now(),
+				"end_time" : 0
+			};
+
+			xml.open("POST", "http://red-velvet-proto.herokuapp.com/chromeext", true);
+			xml.setRequestHeader("Content-type", "application/json");
+			xml.onreadystatechange = function () { //Call a function when the state changes.
+			    if (xml.readyState == 4 && xml.status == 200) {
+			    	console.log(xml.responseText);
+			    	var response = xml.responseText;
+			    	var parsed = JSON.parse(response);
+			    	//console.log(parsed._id);
+			    	ids[tab.id] = parsed._id;
+			    	console.log(ids[tab.id]);
+			        currentID = tab.id;
+			    }
+			}
+			var parameters = JSON.stringify(data);
+			xml.send(parameters);
+		}
+	})
+});
+
+// created tab isn't the active tab. have to fix that!
+
+// when a tab is updated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	// if url changes, post end_time of previous currentID then update currentID
 	// create new post to database
-	var data = {
-		"url" : tab.url,
-		"start_time" : Date.now(),
-		"end_time" : 0
+
+	// post end_time of previous currentID
+	var oldData = {
+		"end_time" : Date.now()
 	};
 
-	xml.open("POST", "http://red-velvet-proto.herokuapp.com/chromeext", true);
+	var db = "http://red-velvet-proto.herokuapp.com/chromeext/" + ids[currentID];
+	xml.open("PUT", db, true);
 	xml.setRequestHeader("Content-type", "application/json");
 	xml.onreadystatechange = function () { //Call a function when the state changes.
 	    if (xml.readyState == 4 && xml.status == 200) {
-	    	console.log(xml.responseText);
-	    	var response = xml.responseText;
-	    	var parsed = JSON.parse(response);
-	    	//console.log(parsed._id);
-	    	ids[tab.id] = parsed._id;
-	    	console.log(ids[tab.id]);
-	        currentID = tab.id;
+	    	console.log(db);
+	    	delete ids[tabId];
 	    }
 	}
-	var parameters = JSON.stringify(data);
-	xml.send(parameters);
+	var sendOld = JSON.stringify(oldData);
+	xml.send(sendOld);
+	
+	// create new post to database and update currentID
+	if (changeInfo.status == "complete") {
+		var data = {
+			"url" : tab.url,
+			"start_time" : Date.now(),
+			"end_time" : 0
+		};
+
+    	xml.open("POST", "http://red-velvet-proto.herokuapp.com/chromeext", true);
+		xml.setRequestHeader("Content-type", "application/json");
+		xml.onreadystatechange = function () { //Call a function when the state changes.
+		    if (xml.readyState == 4 && xml.status == 200) {
+		    	console.log(xml.responseText);
+		    	var response = xml.responseText;
+		    	var parsed = JSON.parse(response);
+		        ids[tab.tabId] = parsed._id;
+				currentID = tab.tabId;
+		    }
+		}
+		var parameters = JSON.stringify(data);
+		xml.send(parameters);
+	}
 });
 
-// when a tab is updated
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-// 	// if url changes, post end_time of previous currentID then update currentID
-// 	// create new post to database
-// 	// else ignore
-// 	if (currentID != changeInfo.url) {
-		
-// 		//posted data to server already
-// 		currentID = changeInfo.url;
-
-// 		// xml.open("POST", changeInfo.url, true);
-// 		// var time = Date.now();
-// 		// var t = time.toString();
-// 		// xml.send(t);
-// 	}
-// })
+// onUpdated is fired constantly...how would i get it so that it only updates
+// the server if the tab has completed loading?
+// -> use changeInfo.status == 'complete'?
+// also, should i add this listener to onCreated? so it gets the final url
+// and not the inbetween urls?
 
 // when a tab is closed
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
